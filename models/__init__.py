e78dcd48d45e772b92e9f0619f81fe4dde90e03a
Anshul Gupta <megaanshulgupta96@gmail.com>
2022-10-19 19:53:42 -0400

    Update models init

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,6 @@ from models.cnn_1 import CNN1
 from models.cnn_2 import CNN2
 from models.resnext_50 import ResNeXT50
 from models.resnet18 import ResNet18
-from models.mobilenetv2 import MobileNetV2
 
 
 def get_model(model_arch):
@@ -13,8 +12,6 @@ def get_model(model_arch):
         model = CNN2()
     elif model_arch == 'r18':
         model = ResNet18()
-    elif model_arch == 'mnv2':
-        model = MobileNetV2()
     elif model_arch == 'rnxt50':
         model = ResNeXT50()
     else:
